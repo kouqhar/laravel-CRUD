2d76e022dc523337f52fc3e1e7264cbab4ccaeb6
kouqhar <naphtaliduniya2@gmail.com>
2026-06-23 06:19:20 +0100

    Resource controller and About Navigation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,6 +17,9 @@
         <div class="navbar-start">
             <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
         </div>
+        <div class="navbar-center">
+            <a href="/about">About</a>
+        </div>
         <div class="navbar-end gap-2">
             <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
